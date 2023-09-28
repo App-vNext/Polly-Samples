95a4657ef1383ec6014bf3111ae3fe0c14da5129
peter.csala <peter.csala@genesys.com>
2023-09-28 11:00:23 +0200

    Migrate sync demos from 01 to 03

--- PollyDemos/Sync/Demo01_RetryNTimes.cs
+++ PollyDemos/Sync/Demo01_RetryNTimes.cs
@@ -1,14 +1,13 @@
-﻿using System.Net;
-using PollyDemos.OutputHelpers;
+﻿using PollyDemos.OutputHelpers;
 
 namespace PollyDemos.Sync
 {
     /// <summary>
-    /// Demonstrates the Retry policy coming into action.
+    /// Demonstrates the Retry strategy coming into action.
     /// Loops through a series of Http requests, keeping track of each requested
     /// item and reporting server failures when encountering exceptions.
-    /// 
-    /// Observations: There's no wait among these retries.  Can be appropriate sometimes.  
+    ///
+    /// Observations: There's no wait among these retries.  Can be appropriate sometimes.
     /// In this case, no wait hasn't given underlying system time to recover, so calls still fail despite retries.
     /// </summary>
     public class Demo01_RetryNTimes : SyncDemo
@@ -23,80 +22,87 @@ namespace PollyDemos.Sync
 
         public override void Execute(CancellationToken cancellationToken, IProgress<DemoProgress> progress)
         {
-            if (progress == null) throw new ArgumentNullException(nameof(progress));
+            ArgumentNullException.ThrowIfNull(progress);
 
-            // Let's call a web api service to make repeated requests to a server. 
+            // Let's call a web api service to make repeated requests to a server.
             // The service is programmed to fail after 3 requests in 5 seconds.
 
             eventualSuccesses = 0;
             retries = 0;
             eventualFailures = 0;
+            totalRequests = 0;
 
             progress.Report(ProgressWithMessage(nameof(Demo01_RetryNTimes)));
             progress.Report(ProgressWithMessage("======"));
             progress.Report(ProgressWithMessage(string.Empty));
 
-            // Let's call a web api service to make repeated requests to a server. 
+            // Let's call a web api service to make repeated requests to a server.
             // The service is programmed to fail after 3 requests in 5 seconds.
 
-            // Define our policy:
-            var policy = Policy.Handle<Exception>().Retry(3, (exception, attempt) =>
+            // Define our strategy:
+            var strategy = new ResiliencePipelineBuilder().AddRetry(new()
             {
-                // This is your new exception handler! 
-                // Tell the user what they've won!
-                progress.Report(ProgressWithMessage("Policy logging: " + exception.Message, Color.Yellow));
-                retries++;
-            });
-
-            using (var client = new WebClient())
-            {
-                var internalCancel = false;
-                totalRequests = 0;
-                // Do the following until a key is pressed
-                while (!internalCancel && !cancellationToken.IsCancellationRequested)
+                ShouldHandle = new PredicateBuilder().Handle<Exception>(),
+                MaxRetryAttempts = 3, // Retry up to 3 times
+                OnRetry = args =>
                 {
-                    totalRequests++;
+                    // Due to fact how we have defined the ShouldHandle this delegate is called only if an exception occurred.
+                    // Please note the ! sign (null-forgiving operator) at the end of the command.
+                    var exception = args.Outcome.Exception!; //The Exception property is nullable
 
-                    try
-                    {
-                        // Retry the following call according to the policy - 3 times.
-                        policy.Execute(
-                            ct => // The Execute() overload takes a CancellationToken, but it happens the executed code does not honour it.
-                            {
-                                // This code is executed within the Policy 
-
-                                // Make a request and get a response
-                                var response =
-                                    client.DownloadString(Configuration.WEB_API_ROOT + "/api/values/" + totalRequests);
-
-                                // Display the response message on the console
-                                progress.Report(ProgressWithMessage("Response : " + response, Color.Green));
-                                eventualSuccesses++;
-                            }
-                            , cancellationToken // The cancellationToken passed in to Execute() enables the policy instance to cancel retries, when the token is signalled.
-                        );
-                    }
-                    catch (Exception e)
-                    {
-                        progress.Report(ProgressWithMessage(
-                            "Request " + totalRequests + " eventually failed with: " + e.Message, Color.Red));
-                        eventualFailures++;
-                    }
-
-                    // Wait half second
-                    Thread.Sleep(500);
-
-                    internalCancel = TerminateDemosByKeyPress && Console.KeyAvailable;
+                    // Tell the user what happened
+                    progress.Report(ProgressWithMessage("Strategy logging: " + exception.Message, Color.Yellow));
+                    retries++;
+                    return default;
                 }
+            }).Build();
+
+            var client = new HttpClient();
+            var internalCancel = false;
+            // Do the following until a key is pressed
+            while (!(internalCancel || cancellationToken.IsCancellationRequested))
+            {
+                totalRequests++;
+
+                try
+                {
+                    // Retry the following call according to the strategy - 3 times.
+                    // the Execute() overload takes a CancellationToken, but the decorated code does not honour it.
+                    strategy.Execute(ct =>
+                        {
+                            // This code is executed within the strategy
+
+                            // Make a request and get a response
+                            var url = $"{Configuration.WEB_API_ROOT}/api/values/{totalRequests}";
+                            // Please note that the cancellation token is not used here.
+                            var response = client.Send(new HttpRequestMessage(HttpMethod.Get, url));
+
+                            // Display the response message on the console
+                            // Please note that the cancellation token is not used here.
+                            using var stream = response.Content.ReadAsStream();
+                            using var streamReader = new StreamReader(stream);
+                            progress.Report(ProgressWithMessage($"Response : {streamReader.ReadToEnd()}", Color.Green));
+                            eventualSuccesses++;
+
+                        }, cancellationToken); // The cancellationToken passed in to Execute() enables the strategy to cancel retries, when the token is signalled.
+                }
+                catch (Exception e)
+                {
+                    progress.Report(ProgressWithMessage($"Request {totalRequests} eventually failed with: {e.Message}", Color.Red));
+                    eventualFailures++;
+                }
+
+                Thread.Sleep(500);
+                internalCancel = TerminateDemosByKeyPress && Console.KeyAvailable;
             }
         }
 
-        public override Statistic[] LatestStatistics => new[]
+        public override Statistic[] LatestStatistics => new Statistic[]
         {
-            new Statistic("Total requests made", totalRequests),
-            new Statistic("Requests which eventually succeeded", eventualSuccesses, Color.Green),
-            new Statistic("Retries made to help achieve success", retries, Color.Yellow),
-            new Statistic("Requests which eventually failed", eventualFailures, Color.Red),
+            new("Total requests made", totalRequests),
+            new("Requests which eventually succeeded", eventualSuccesses, Color.Green),
+            new("Retries made to help achieve success", retries, Color.Yellow),
+            new("Requests which eventually failed", eventualFailures, Color.Red),
         };
     }
 }
--- PollyDemos/Sync/Demo03_WaitAndRetryNTimes_WithEnoughRetries.cs
+++ PollyDemos/Sync/Demo03_WaitAndRetryNTimes_WithEnoughRetries.cs
@@ -1,13 +1,12 @@
-﻿using System.Net;
-using PollyDemos.OutputHelpers;
+﻿using PollyDemos.OutputHelpers;
 
 namespace PollyDemos.Sync
 {
     /// <summary>
-    /// Demonstrates the WaitAndRetry policy.
+    /// Demonstrates the Retry strategy with delays between retry attempts.
     /// Loops through a series of Http requests, keeping track of each requested
     /// item and reporting server failures when encountering exceptions.
-    /// 
+    ///
     /// Observations: We now have waits and enough retries: all calls now succeed!  Yay!
     /// But we kind-a had to guess how many retries would be enough before the server responded again ...
     /// (and we're hammering that server with retries)
@@ -24,84 +23,90 @@ namespace PollyDemos.Sync
 
         public override void Execute(CancellationToken cancellationToken, IProgress<DemoProgress> progress)
         {
-            if (progress == null) throw new ArgumentNullException(nameof(progress));
+            ArgumentNullException.ThrowIfNull(progress);
 
-            // Let's call a web api service to make repeated requests to a server. 
+            // Let's call a web api service to make repeated requests to a server.
             // The service is programmed to fail after 3 requests in 5 seconds.
 
             eventualSuccesses = 0;
             retries = 0;
             eventualFailures = 0;
+            totalRequests = 0;
 
             progress.Report(ProgressWithMessage(nameof(Demo03_WaitAndRetryNTimes_WithEnoughRetries)));
             progress.Report(ProgressWithMessage("======"));
             progress.Report(ProgressWithMessage(string.Empty));
 
-            // Let's call a web api service to make repeated requests to a server. 
+            // Let's call a web api service to make repeated requests to a server.
             // The service is programmed to fail after 3 requests in 5 seconds.
 
-            // Define our policy:
-            var policy = Policy.Handle<Exception>().WaitAndRetry(
-                20, // Retry up to 20 times! - should be enough that we eventually succeed.
-                attempt => TimeSpan.FromMilliseconds(200), // Wait 200ms between each try.
-                (exception, calculatedWaitDuration) => // Capture some info for logging!
-                {
-                    // This is your new exception handler! 
-                    // Tell the user what they've won!
-                    progress.Report(ProgressWithMessage("Policy logging: " + exception.Message, Color.Yellow));
-                    retries++;
-                });
-
-            using (var client = new WebClient())
+            // Define our strategy:
+            var strategy = new ResiliencePipelineBuilder().AddRetry(new()
             {
-                totalRequests = 0;
-                var internalCancel = false;
-
-                // Do the following until a key is pressed
-                while (!internalCancel && !cancellationToken.IsCancellationRequested)
+                ShouldHandle = new PredicateBuilder().Handle<Exception>(),
+                MaxRetryAttempts = 20, // Retry up to 20 times! - should be enough that we eventually succeed.
+                Delay = TimeSpan.FromMilliseconds(200),  // Wait 200ms between each try
+                OnRetry = args =>
                 {
-                    totalRequests++;
+                    // Due to fact how we have defined the ShouldHandle this delegate is called only if an exception occurred.
+                    // Please note the ! sign (null-forgiving operator) at the end of the command.
+                    var exception = args.Outcome.Exception!; //The Exception property is nullable
 
-                    try
-                    {
-                        // Retry the following call according to the policy - 20 times.
-                        policy.Execute(
-                            ct => // The Execute() overload takes a CancellationToken, but it happens the executed code does not honour it.
-                            {
-                                // This code is executed within the Policy 
-
-                                // Make a request and get a response
-                                var response =
-                                    client.DownloadString(Configuration.WEB_API_ROOT + "/api/values/" + totalRequests);
-
-                                // Display the response message on the console
-                                progress.Report(ProgressWithMessage("Response : " + response, Color.Green));
-                                eventualSuccesses++;
-                            }
-                            , cancellationToken // The cancellationToken passed in to Execute() enables the policy instance to cancel retries, when the token is signalled.
-                        );
-                    }
-                    catch (Exception e)
-                    {
-                        progress.Report(ProgressWithMessage(
-                            "Request " + totalRequests + " eventually failed with: " + e.Message, Color.Red));
-                        eventualFailures++;
-                    }
-
-                    // Wait half second before the next request.
-                    Thread.Sleep(500);
-
-                    internalCancel = TerminateDemosByKeyPress && Console.KeyAvailable;
+                    // Tell the user what happened
+                    progress.Report(ProgressWithMessage("Strategy logging: " + exception.Message, Color.Yellow));
+                    retries++;
+                    return default;
                 }
+            }).Build();
+
+
+            var client = new HttpClient();
+            var internalCancel = false;
+
+            // Do the following until a key is pressed
+            while (!(internalCancel || cancellationToken.IsCancellationRequested))
+            {
+                totalRequests++;
+
+                try
+                {
+                    // Retry the following call according to the strategy - 20 times.
+                    // the Execute() overload takes a CancellationToken, but the decorated code does not honour it.
+                    strategy.Execute(
+                        ct =>
+                        {
+                            // This code is executed within the strategy
+
+                            // Make a request and get a response
+                            var url = $"{Configuration.WEB_API_ROOT}/api/values/{totalRequests}";
+                            // Please note that the cancellation token is not used here.
+                            var response = client.Send(new HttpRequestMessage(HttpMethod.Get, url));
+
+                            // Display the response message on the console
+                            // Please note that the cancellation token is not used here.
+                            using var stream = response.Content.ReadAsStream();
+                            using var streamReader = new StreamReader(stream);
+                            progress.Report(ProgressWithMessage($"Response : {streamReader.ReadToEnd()}", Color.Green));
+                            eventualSuccesses++;
+                        }, cancellationToken); // The cancellationToken passed in to Execute() enables the strategy to cancel retries, when the token is signalled.
+                }
+                catch (Exception e)
+                {
+                    progress.Report(ProgressWithMessage($"Request {totalRequests} eventually failed with: {e.Message}", Color.Red));
+                    eventualFailures++;
+                }
+
+                Thread.Sleep(500);
+                internalCancel = TerminateDemosByKeyPress && Console.KeyAvailable;
             }
         }
 
-        public override Statistic[] LatestStatistics => new[]
+        public override Statistic[] LatestStatistics => new Statistic[]
         {
-            new Statistic("Total requests made", totalRequests),
-            new Statistic("Requests which eventually succeeded", eventualSuccesses, Color.Green),
-            new Statistic("Retries made to help achieve success", retries, Color.Yellow),
-            new Statistic("Requests which eventually failed", eventualFailures, Color.Red),
+            new("Total requests made", totalRequests),
+            new("Requests which eventually succeeded", eventualSuccesses, Color.Green),
+            new("Retries made to help achieve success", retries, Color.Yellow),
+            new("Requests which eventually failed", eventualFailures, Color.Red),
         };
     }
 }
