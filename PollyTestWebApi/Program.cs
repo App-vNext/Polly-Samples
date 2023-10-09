22a544083cd575cd9a3bee21583212768edebfb4
peter.csala <peter.csala@genesys.com>
2023-10-09 12:13:56 +0200

    Use top-level program

--- PollyTestWebApi/Program.cs
+++ PollyTestWebApi/Program.cs
@@ -1,36 +1,23 @@
-using System;
-using System.Threading;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.RateLimiting;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace PollyTestWebApi
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddRateLimiter(limiterOptions =>
 {
-    public class Program
+    limiterOptions.AddFixedWindowLimiter("ThreeRequestsPerFiveSeconds", windowOptions =>
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllers();
-            builder.Services.AddRateLimiter(limiterOptions =>
-            {
-                limiterOptions.AddFixedWindowLimiter("ThreeRequestsPerFiveSeconds", windowOptions =>
-                {
-                    windowOptions.PermitLimit = 3;
-                    windowOptions.Window = TimeSpan.FromSeconds(5);
-                });
-                limiterOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
-                limiterOptions.OnRejected = async (context, _) =>
-                {
-                    await context.HttpContext.Response.WriteAsync("Too many requests have received. Request refused.", CancellationToken.None);
-                };
-            });
-            var app = builder.Build();
+        windowOptions.PermitLimit = 3;
+        windowOptions.Window = TimeSpan.FromSeconds(5);
+    });
+    limiterOptions.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    limiterOptions.OnRejected = async (context, _) =>
+    {
+        await context.HttpContext.Response.WriteAsync("Too many requests have received. Request refused.", CancellationToken.None);
+    };
+});
+var app = builder.Build();
 
-            app.MapControllers();
-            app.UseRateLimiter();
-            app.Run("http://localhost:45179");
-        }
-    }
-}
+app.MapControllers();
+app.UseRateLimiter();
+app.Run("http://localhost:45179");
+
