40ad6314d696aa6eb9c9a64f869ec776fe567aea
peter.csala <peter.csala@genesys.com>
2023-10-10 15:22:48 +0200

    Migrate UI

--- PollyTestClientWpf/MainWindow.xaml.cs
+++ PollyTestClientWpf/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using PollyDemos;
 using PollyDemos.Async;
 using PollyDemos.OutputHelpers;
@@ -22,88 +10,84 @@ using Color = PollyDemos.OutputHelpers.Color;
 
 namespace PollyTestClientWpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private CancellationTokenSource cancellationSource;
-        private CancellationToken cancellationToken;
+        private CancellationTokenSource? cancellationSource;
 
-        private static readonly Type[] availableDemoTypes = Assembly.GetAssembly(typeof(DemoBase)).GetTypes()
-            .Where(t => t.IsSubclassOf(typeof(DemoBase)) && !t.IsAbstract).ToArray();
+        private readonly Type[] availableDemoTypes;
 
-        private readonly object lockObject = new object();
+        private readonly object lockObject = new();
 
-        private Statistic[] closingStatistics = new Statistic[0];
+        private Statistic[] closingStatistics = Array.Empty<Statistic>();
 
         private const int MaxStatisticsToShow = 9;
         private const string StatisticBoxPrefix = "Statistic";
         private const string StatisticLabelPrefix = "StatisticLabel";
 
-        private Progress<DemoProgress> progress;
+        private readonly Progress<DemoProgress> progress;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
-
-            PlayButton.Click += (sender, args) => PlayButton_Click();
-            StopButton.Click += (sender, args) => StopButton_Click();
-            ClearButton.Click += (sender, args) => ClearButton_Click();
+            availableDemoTypes = Assembly.GetAssembly(typeof(DemoBase))!
+               .GetTypes()
+               .Where(t => t.IsSubclassOf(typeof(DemoBase)) && !t.IsAbstract)
+               .ToArray();
 
             progress = new Progress<DemoProgress>();
-            progress.ProgressChanged += (sender, progressArgs) =>
+            progress.ProgressChanged += (sender, args) =>
             {
                 Output.Dispatcher.Invoke(() =>
                 {
-                    WriteMultiLineInColor(progressArgs.Messages);
-
-                    UpdateStatistics(progressArgs.Statistics);
+                    WriteMultiLineInColor(args.Messages);
+                    UpdateStatistics(args.Statistics);
                 });
 
-                closingStatistics = progressArgs.Statistics;
+                closingStatistics = args.Statistics;
+            };
+
+            TaskScheduler.UnobservedTaskException += (sender, args) =>
+            {
+                Output.Dispatcher.Invoke(() =>
+                {
+                    StopButton_Click(this, new());
+                    WriteLineInColor($"Unobserved task exception: {args.Exception.Flatten()}.", Color.Red);
+                });
+
+                args.SetObserved();
             };
 
             Demo.SelectedIndex = 0;
-
-            ClearButton_Click();
         }
 
-        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
-        {
-            Output.Dispatcher.Invoke(() =>
-            {
-                StopButton_Click();
-                WriteLineInColor($"Unobserved task exception: {e.Exception.Flatten()}.", Color.Red);
-            });
+        private Type? GetDemoType(string demoName)
+            => availableDemoTypes.SingleOrDefault(type => type.Name == demoName);
 
-            e.SetObserved();
-        }
-
-        private static Type GetDemoType(string demoName)
-        {
-            return availableDemoTypes.SingleOrDefault(t => t.Name == demoName);
-        }
-
-        private void ClearButton_Click()
+        private void ClearButton_Click(object sender, RoutedEventArgs args)
         {
             Output.Document = new FlowDocument();
-            UpdateStatistics(new Statistic[0]);
+            for (var i = 0; i < MaxStatisticsToShow; i++)
+            {
+                var statSuffix = $"{i:00}";
+                var label = (Label)FindName(StatisticLabelPrefix + statSuffix);
+                label.Content = string.Empty;
+
+                var statBox = (TextBox)FindName(StatisticBoxPrefix + statSuffix);
+                statBox.Text = string.Empty;
+            }
         }
 
-        private void PlayButton_Click()
+        private void PlayButton_Click(object sender, RoutedEventArgs args)
         {
+            ClearButton_Click(this, new());
             StopButton.IsEnabled = true;
             PlayButton.IsEnabled = false;
 
             cancellationSource = new CancellationTokenSource();
+            var cancellationToken = cancellationSource.Token;
 
-            cancellationToken = cancellationSource.Token;
-
-            var selectedItem = Demo.SelectedItem as ComboBoxItem;
-            if (selectedItem == null)
+            if (Demo.SelectedItem is not ComboBoxItem selectedItem)
             {
                 WriteLineInColor("No demo selected.", Color.Red);
                 cancellationSource.Cancel();
@@ -116,20 +100,19 @@ namespace PollyTestClientWpf
             {
                 WriteLineInColor($"Unable to identify demo: {demoName}", Color.Red);
                 cancellationSource.Cancel();
+                return;
             }
-            else if (demoType.IsSubclassOf(typeof(SyncDemo)))
+
+            if (demoType.IsSubclassOf(typeof(SyncDemo)))
             {
-                SyncDemo demoInstance;
+                SyncDemo? demoInstance = null;
                 try
                 {
                     demoInstance = Activator.CreateInstance(demoType) as SyncDemo;
                 }
-                catch (Exception)
-                {
-                    demoInstance = null;
-                }
+                catch (Exception) {}
 
-                if (demoInstance == null)
+                if (demoInstance is null)
                 {
                     WriteLineInColor($"Unable to instantiate demo: {demoName}", Color.Red);
                     cancellationSource.Cancel();
@@ -138,60 +121,61 @@ namespace PollyTestClientWpf
 
                 try
                 {
-                    Task.Factory.StartNew(() => demoInstance.Execute(cancellationToken, progress), cancellationToken,
-                            TaskCreationOptions.LongRunning, TaskScheduler.Default)
-                        .ContinueWith(t =>
-                        {
-                            if (t.IsCanceled)
-                                WriteLineInColor($"Demo was canceled: {demoName}", Color.Red);
-                            else if (t.IsFaulted)
-                                WriteLineInColor($"Demo {demoName} threw exception: {t.Exception.ToString()}",
-                                    Color.Red);
-                        }, CancellationToken.None, TaskContinuationOptions.NotOnRanToCompletion, TaskScheduler.Default);
+                    Task.Factory
+                        .StartNew(
+                            () => demoInstance.Execute(cancellationToken, progress),
+                            cancellationToken, TaskCreationOptions.LongRunning, TaskScheduler.Default)
+                        .ContinueWith(t => HandleFailedTask(t, demoName),
+                            CancellationToken.None, TaskContinuationOptions.NotOnRanToCompletion, TaskScheduler.Default);
                 }
                 catch (Exception e)
                 {
                     WriteLineInColor($"Demo {demoName} threw exception: {e}", Color.Red);
                 }
+
+                return;
             }
-            else if (demoType.IsSubclassOf(typeof(AsyncDemo)))
+
+            if (demoType.IsSubclassOf(typeof(AsyncDemo)))
             {
-                AsyncDemo demoInstance;
+                AsyncDemo? demoInstance = null;
                 try
                 {
                     demoInstance = Activator.CreateInstance(demoType) as AsyncDemo;
                 }
-                catch (Exception)
-                {
-                    demoInstance = null;
-                }
+                catch (Exception) {}
 
-                if (demoInstance == null)
+                if (demoInstance is null)
                 {
                     WriteLineInColor($"Unable to instantiate demo: {demoName}", Color.Red);
                     cancellationSource.Cancel();
                     return;
                 }
 
-                Task.Factory.StartNew(() => demoInstance.ExecuteAsync(cancellationToken, progress), cancellationToken,
-                        TaskCreationOptions.LongRunning, TaskScheduler.Default)
+                Task.Factory
+                    .StartNew(
+                        () => demoInstance.ExecuteAsync(cancellationToken, progress),
+                        cancellationToken, TaskCreationOptions.LongRunning, TaskScheduler.Default)
                     .Unwrap()
-                    .ContinueWith(t =>
-                    {
-                        if (t.IsCanceled)
-                            WriteLineInColor($"Demo was canceled: {demoName}", Color.Red);
-                        else if (t.IsFaulted)
-                            WriteLineInColor($"Demo {demoName} threw exception: {t.Exception.ToString()}", Color.Red);
-                    }, CancellationToken.None, TaskContinuationOptions.NotOnRanToCompletion, TaskScheduler.Default);
+                    .ContinueWith(t => HandleFailedTask(t, demoName),
+                        CancellationToken.None, TaskContinuationOptions.NotOnRanToCompletion, TaskScheduler.Default);
+
+                return;
             }
-            else
-            {
-                WriteLineInColor($"Unable to identify demo as either sync or async demo: {demoName}", Color.Red);
-                cancellationSource.Cancel();
-            }
+
+            WriteLineInColor($"Unable to identify demo as either sync or async demo: {demoName}", Color.Red);
+            cancellationSource.Cancel();
         }
 
-        private void StopButton_Click()
+        private void HandleFailedTask(Task task, string demoName)
+        {
+            var message = string.Empty;
+            if (task.IsCanceled) message = $"Demo was canceled: {demoName}";
+            if (task.IsFaulted) message = $"Demo {demoName} threw exception: {task.Exception}";
+            WriteLineInColor(message, Color.Red);
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs args)
         {
             StopButton.IsEnabled = false;
             PlayButton.IsEnabled = true;
@@ -212,12 +196,14 @@ namespace PollyTestClientWpf
                 return;
             }
 
-            // Output closing statistics.
             if (closingStatistics.Any())
             {
                 int longestDescription = closingStatistics.Max(s => s.Description.Length);
                 foreach (Statistic stat in closingStatistics)
+                {
                     WriteLineInColor(stat.Description.PadRight(longestDescription) + ": " + stat.Value, stat.Color);
+                }
+
             }
 
             cancellationSource.Dispose();
@@ -225,31 +211,23 @@ namespace PollyTestClientWpf
 
         private void Demo_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var comboBox = sender as ComboBox;
-
-            var demoName = (comboBox.SelectedItem as ComboBoxItem).Name;
+            Description.Text = string.Empty;
+            var comboBox = (ComboBox)sender;
+            var demoName = ((ComboBoxItem)comboBox.SelectedItem).Name;
             var demoType = GetDemoType(demoName);
             if (demoType == null)
             {
-                Description.Text = string.Empty;
                 return;
             }
 
-            DemoBase demoInstance;
+            DemoBase? demoInstance = null;
             try
             {
                 demoInstance = Activator.CreateInstance(demoType) as DemoBase;
             }
-            catch (Exception)
-            {
-                demoInstance = null;
-            }
+            catch (Exception) { }
 
-            if (demoInstance == null)
-            {
-                Description.Text = string.Empty;
-                return;
-            }
+            if (demoInstance is null) return;
 
             Description.Text = demoInstance.Description;
         }
@@ -259,20 +237,22 @@ namespace PollyTestClientWpf
             Output.Dispatcher.Invoke(() =>
             {
                 lock (lockObject) // Locking helps avoid the color of one message leaking onto another, in multi-threaded callbacks.
-                    foreach (ColoredMessage message in messages)
+                {
+                    foreach (var message in messages)
                     {
                         var newText = new TextRange(Output.Document.ContentEnd, Output.Document.ContentEnd)
                         {
-                            Text = message.Message + "\n"
+                            Text = $"{message.Message}\n\r"
                         };
                         newText.ApplyPropertyValue(TextElement.ForegroundProperty, message.Color.ToBrushColor());
                     }
+                }
 
                 Output.ScrollToEnd();
             });
         }
 
-        public void WriteLineInColor(string msg, Color color)
+        public void WriteLineInColor(string message, Color color)
         {
             Output.Dispatcher.Invoke(() =>
             {
@@ -280,39 +260,39 @@ namespace PollyTestClientWpf
                 {
                     var newText = new TextRange(Output.Document.ContentEnd, Output.Document.ContentEnd)
                     {
-                        Text = msg + "\n"
+                        Text = $"{message}\n\r"
                     };
                     newText.ApplyPropertyValue(TextElement.ForegroundProperty, color.ToBrushColor());
-                    Output.ScrollToEnd();
                 }
+
+                Output.ScrollToEnd();
             });
         }
 
-        private void UpdateStatistics(Statistic[] stats)
+        private void UpdateStatistics(Statistic[] statistics)
         {
             Output.Dispatcher.Invoke(() =>
             {
-                var statisticsToShow = stats.Length;
+                var statisticsToShow = statistics.Length;
                 for (var i = 0; i < MaxStatisticsToShow; i++)
                 {
                     var statSuffix = $"{i:00}";
                     var label = (Label)FindName(StatisticLabelPrefix + statSuffix);
                     var statBox = (TextBox)FindName(StatisticBoxPrefix + statSuffix);
 
-                    if (i < statisticsToShow)
-                    {
-                        Statistic statistic = stats[i];
-                        label.Content = statistic.Description;
-                        statBox.Foreground = statistic.Color.ToBrushColor();
-                        statBox.Text = statistic.Value.ToString().PadLeft(3);
-                        label.Visibility = Visibility.Visible;
-                        statBox.Visibility = Visibility.Visible;
-                    }
-                    else
+                    if (i >= statisticsToShow)
                     {
                         label.Visibility = Visibility.Hidden;
                         statBox.Visibility = Visibility.Hidden;
+                        return;
                     }
+
+                    var statistic = statistics[i];
+                    label.Content = statistic.Description;
+                    statBox.Foreground = statistic.Color.ToBrushColor();
+                    statBox.Text = statistic.Value.ToString().PadLeft(3);
+                    label.Visibility = Visibility.Visible;
+                    statBox.Visibility = Visibility.Visible;
                 }
             });
         }
