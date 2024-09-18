4368ec79ab20f68d4c44ab3995fb223fa91361c5
martincostello <martin@martincostello.com>
2024-09-18 12:25:25 +0100

    Use Spectre.Console
    Use Spectre.Console for selecting the demo to run.

--- PollyTestClientConsole/Menu/ConsoleMenuItem.cs
+++ PollyTestClientConsole/Menu/ConsoleMenuItem.cs
@@ -1,3 +1,6 @@
 namespace PollyTestClientConsole.Menu;
 
-public record ConsoleMenuItem(string Name, Action Handler);
+public record ConsoleMenuItem(string Name, Action Handler)
+{
+    public override string ToString() => Name;
+}
